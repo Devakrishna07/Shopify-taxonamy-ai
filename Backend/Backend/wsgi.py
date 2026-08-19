f916e9cf869f859b721959d2ed2594cf9dfa7cc8
DevaKrishna07 <devakrishna1418@gmail.com>
2026-08-20 04:30:50 +0530

    merge resolved

--- Backend/Backend/wsgi.py
+++ Backend/Backend/wsgi.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """
 WSGI config for Backend project.
 
@@ -15,21 +15,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for Backend project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
-
-application = get_wsgi_application()
->>>>>>> 65164da39133587bb6c55d572e2bbc5cde8b4f6d
